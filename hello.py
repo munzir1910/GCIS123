b822f247ee12934c5a4f6d61ab4c2be26fa3a9ed
munzir1910 <mak4339@g.rit.edu>
2024-09-11 13:59:58 +0400

    hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
+# here im making a function that prints "hello, world"
 def hello_world():
     print("Hello, World!")
 
+# here im making a function that inputs a question then prints the input
 def hello_you():
     name = input("What is your name?:")
     print("Hello!,", name)
-
+    
+ # here im writing the names of the functions to let the program run the code
 hello_world()
 hello_you()
